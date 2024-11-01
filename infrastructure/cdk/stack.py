695feb573b757bf989b42c568cfb77c37cf21205
simonthedeveloper <mail@simon-neumann.com>
2024-11-01 19:08:11 +0000

    AWS infrastructure

--- infrastructure/cdk/stack.py
+++ infrastructure/cdk/stack.py
@@ -1,11 +1,16 @@
-from aws_cdk import core
+from aws_cdk import Stack
 import aws_cdk.aws_dynamodb as dynamodb
+from constructs import Construct  # constructs is now the base library for AWS CDK constructs
+from dotenv import load_dotenv
 
-class AirlineTestStack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+
+
+class AirlineTestStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         table = dynamodb.Table(
             self, "FlightsTable",
             partition_key=dynamodb.Attribute(name="flight_id", type=dynamodb.AttributeType.STRING)
         )
+
